5bc91a6a495e7cfb417e1e3dcdd2fcfa473ad7c5
Amit Bajaj <amitbajaj100@yahoo.com>
2019-09-13 14:14:38 +0300

    Basic socket.io code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const PORT = process.env.PORT || 5000
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 
-http.listen(PORT); //listen to port 8080
+http.listen(PORT); //listen to port (either the system or local 5000)
 
 function handler (req, res) { //create server
   fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
@@ -15,3 +16,16 @@ function handler (req, res) { //create server
     return res.end();
   });
 }
+
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+    var lightvalue = 0; //static variable for current status
+    socket.on('light', function(data) { //get light switch status from client
+      lightvalue = data;
+      if (lightvalue) {
+        console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
+        socket.emit(0);
+      }else{
+          socket.emit(1);
+      }
+    });
+  });
