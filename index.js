05803af94e23a879820a6698f90b2dfb827b28de
Amit Bajaj <amitbajaj100@yahoo.com>
2019-09-28 14:12:05 +0300

    Arrays and on/off functions with variable speed

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ var http = require('http').createServer(handler); //require http server, and cre
 var fs = require('fs'); //require filesystem module
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 var Gpio = require('onoff').Gpio; //require onoff module to interact with GPIO header
-var R1 = new Gpio(26, 'out'); //use GPIO pin 26 for Relay 1, and specify that it is output
-var R2 = new Gpio(19, 'out'); //use GPIO pin 19 for Relay 2, and specify that it is output
-var R3 = new Gpio(13, 'out'); //use GPIO pin 13 for Relay 3, and specify that it is output
-var R4 = new Gpio(6, 'out');  //use GPIO pin  6 for Relay 4, and specify that it is output
+var R = Array();
+R[0] = new Gpio(26, 'out'); //use GPIO pin 26 for Relay 1, and specify that it is output
+R[1] = new Gpio(19, 'out'); //use GPIO pin 19 for Relay 2, and specify that it is output
+R[2] = new Gpio(13, 'out'); //use GPIO pin 13 for Relay 3, and specify that it is output
+R[3] = new Gpio(6, 'out');  //use GPIO pin  6 for Relay 4, and specify that it is output
+var speed = 500; //Current interval between on and off sequences
+var t; //the Interval Timer handle
+var counter = 1; //Addition factor
+var direction = 1; //addition direction (+1 to move forward, -1 to move backwards)
+var switches = R.length; //Number of relays.
+var isActive = false; //Status of relays
 
 http.listen(PORT); //listen to port (either the system or local 5000)
 
@@ -23,49 +30,51 @@ function handler (req, res) { //create server
 }
 
 io.sockets.on('connection', function (socket) {// WebSocket Connection
-    var lightvalue = 0; //static variable for current status
-    socket.on('light', function(data) { //get light switch status from client
-      lightvalue = data;
-      if (lightvalue>=10) {
-        //console.log(1); //turn LED on or off, for now we will just show it in console.log
-        lightvalue = -1;
-        socket.emit("toggle",1);
-        R4.writeSync(0);
-      }else if(lightvalue==5){
-        R4.writeSync(1);
-      }
-      socket.emit("light",lightvalue+1);
+    // var lightvalue = 0; //static variable for current status
+    // socket.on('light', function(data) { //get light switch status from client
+    //   lightvalue = data;
+    //   if (lightvalue>=10) {
+    //     //console.log(1); //turn LED on or off, for now we will just show it in console.log
+    //     lightvalue = -1;
+    //     socket.emit("toggle",1);
+    //   }else if(lightvalue==5){
+    //   }
+    //   socket.emit("light",lightvalue+1);
+    // });
+    socket.on("status",function(data){
+      socket.emit("status",isActive);
+    });
+    socket.on("activate", function(data){
+      isActive = true;
+      setTimeout(blink,speed);
+      socket.emit("activated",speed);
+    });
+    socket.on("deactivate",function(data){
+      switchoff();
+      socket.emit("deactivated",null);
+    });
+    socket.on("setspeed",function(data){
+      speed=parseInt(data);
     });
   });
 
-var t = setInterval(blink,500);
-var i = 1;
+function switchoff(){
+  //console.log("Stopping the loop..."+counter);
+  isActive = false;
+  for(i=0;i<switches;i++){
+    R[i].writeSync(0);
+  }
+}
 function blink(){
-  if(i>=5) i=1;
-  switch(i++){
-    case 1:
-      R1.writeSync(0);
-      R2.writeSync(1);
-      R3.writeSync(1);
-      R4.writeSync(1);
-      break;
-    case 2:
-      R1.writeSync(1);
-      R2.writeSync(0);
-      R3.writeSync(1);
-      R4.writeSync(1);
-      break;
-    case 3:
-      R1.writeSync(1);
-      R2.writeSync(1);
-      R3.writeSync(0);
-      R4.writeSync(1);
-      break;
-    case 4:
-      R1.writeSync(1);
-      R2.writeSync(1);
-      R3.writeSync(1);
-      R4.writeSync(0);
-      break;
+  for(i=0;i<switches;i++){
+    R[i].writeSync(counter-i-1==0?0:1);
+  }
+  if((counter+direction)>switches | counter+direction<1) direction*=-1;
+  counter+=direction;
+  if(isActive){
+    //console.log("Looping...."+counter);
+    setTimeout(blink,speed);
+  }else{
+    switchoff();
   }
 }
